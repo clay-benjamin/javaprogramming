0890763ece83a7418bfb2ca1dced86c05d91b423
clay-benjamin <clay.benjamin.pro@gmail.com>
2021-02-12 23:23:58 +0100

    From VSCode

--- javaCommitToGit/src/com/git/commit/JavaCommit.java
+++ javaCommitToGit/src/com/git/commit/JavaCommit.java
@@ -3,5 +3,6 @@ package com.git.commit;
 public class JavaCommit {
 	public static void main(String[] args) {
 		System.out.print("Did this make it to GitHub?...");
+		System.out.print("Change made in VSCode...");
 	}
 }
