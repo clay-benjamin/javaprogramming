4e5df5f464f875c9f1cd24886ed28d4719b36c43
clay-benjamin <clay.benjamin.pro@gmail.com>
2021-02-25 23:27:12 +0100

    Also from VS Code

--- JavaPostSetup/src/com/setup/java/JavaPostSetupCheck.java
+++ JavaPostSetup/src/com/setup/java/JavaPostSetupCheck.java
@@ -2,8 +2,8 @@ package com.setup.java;
 
 public class JavaPostSetupCheck {
 	public static void main(String[] args) {
-		System.out.println("Did this make it to GitHub?...");
-		System.out.println("Change made in VSCode...");
+		System.out.print("Did this make it to GitHub?...");
+		System.out.print("Change made in VSCode...");
 		System.out.println("New Project - Upload/Push...");
 
 	}
