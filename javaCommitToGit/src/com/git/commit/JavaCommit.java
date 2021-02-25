c3066151fea21aae29e7601b6a46bc8226ad33d1
clay-benjamin <clay.benjamin.pro@gmail.com>
2021-02-25 22:36:52 +0100

    After Clone, upload

--- javaCommitToGit/src/com/git/commit/JavaCommit.java
+++ javaCommitToGit/src/com/git/commit/JavaCommit.java
@@ -4,5 +4,6 @@ public class JavaCommit {
 	public static void main(String[] args) {
 		System.out.print("Did this make it to GitHub?...");
 		System.out.print("Change made in VSCode...");
+		System.out.print("Clone from Git to Local...");
 	}
 }
