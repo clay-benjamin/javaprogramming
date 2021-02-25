9957eb4a7c368562ffb0d0c98b9553d36fa639b1
clay-benjamin <clay.benjamin.pro@gmail.com>
2021-02-25 23:58:36 +0100

    VS Code - 23:58

--- JavaPostSetup/src/com/setup/java/JavaPostSetupCheck.java
+++ JavaPostSetup/src/com/setup/java/JavaPostSetupCheck.java
@@ -4,7 +4,7 @@ public class JavaPostSetupCheck {
 	public static void main(String[] args) {
 		System.out.print("Did this make it to GitHub?...");
 		System.out.print("Change made in VSCode...");
-		System.out.println("New Project");
+		System.out.println("VS Code");
 
 	}
 }
