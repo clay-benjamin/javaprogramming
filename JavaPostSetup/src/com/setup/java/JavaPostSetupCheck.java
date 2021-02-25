03b221f69fa825783a9c3a59b4ee76b579330a4a
clay-benjamin <clay.benjamin.pro@gmail.com>
2021-02-25 22:48:13 +0100

    new project, Commit to GH

---
+++ JavaPostSetup/src/com/setup/java/JavaPostSetupCheck.java
@@ -0,0 +1,10 @@
+package com.setup.java;
+
+public class JavaPostSetupCheck {
+	public static void main(String[] args) {
+		System.out.println("Did this make it to GitHub?...");
+		System.out.println("Change made in VSCode...");
+		System.out.println("New Project - Upload/Push...");
+
+	}
+}
